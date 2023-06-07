22163a26bb9261e1c3d12392846419ecb014da15
marianalbuquerque <131072436+marianalbuquerque@users.noreply.github.com>
2023-06-07 08:45:22 -0300

    Add files via upload

---
+++ mari/script1.js
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>JavaScript Functions</h1>
+
+<p>Call a function which performs a calculation and returns the result:</p>
+
+<p>Enter two numbers:</p>
+<input type="number" id="num1">
+<input type="number" id="num2">
+<button onclick="calculate()">Calculate</button>
+
+<p id="demo"></p>
+
+<script>
+function myFunction(p1, p2)
+
+function myFunction(p1, p)
+
+function myFunction(p1)
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+function calculate() { 
+
+ // Obter os valores dos números digitados
+ let num1 = parseInt(document.getElementById)"num1"
+
+
+}
+  
+
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+function calculate() {
+  // Obter os valores dos números digitados
+  let num1 = parseInt(document.getElementById("num1").value);
+  let num2 = parseInt(document.getElementById("num2").value);
+
+  // Chamar a função myFunction com os números fornecidos
+  let result = myFunction(num1, num2);
+
+  // Exibir o resultado na página
+  document.getElementById("demo")
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+function calculate() {
+  // Obter os valores dos números digitados
+  let num1 = parseInt(document.getElementById("num1").value);
+  let num2 = parseInt(document.getElementById("num2").value);
+
+  // Chamar a função myFunction com os números fornecidos
+  let result = myFunction(num1, num2);
+
+  // Exibir o resultado na página
+  document.getElementById("demo").innerHTML = result;
+}
+</script>
+
+
+
+<
+
+
+<body>
+
+<
+</html>
+
+``
